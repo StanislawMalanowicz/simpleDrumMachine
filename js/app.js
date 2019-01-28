231ab194c2777265b117e4e21ac52644b7468b17
StanislawMalanowicz <beben86@wp.pl>
2019-01-28 21:41:30 +0100

    first commit

--- js/app.js
+++ js/app.js
@@ -1,20 +1,35 @@
 const head = document.querySelector("h1")
 const kick = document.querySelector(".kick")
 const snare = document.querySelector(".snare")
+const play = document.querySelector(".play")
+const kickDrum = "kick";
+const snareDrum = "snare";
 
-head.addEventListener("click", () =>{
-    console.log("dziala js")
-})
+
+    createjs.Sound.registerSound("audio/kick.wav", kickDrum);
+    createjs.Sound.registerSound("audio/snare.wav", snareDrum);
+
+
+// play.addEventListener("click", ()=>{
+//     console.log("play")
+//     createjs.Sound.play(kickDrum);
+// })
+
+// head.addEventListener("click", () =>{
+//     console.log("dziala js")
+// })
 
 document.addEventListener("keydown", (e)=>{
     console.log(e.which, e.key)
     if(e.which == 81){
         console.log("boom")
         // kick.classList.remove("kick")
+        createjs.Sound.play(kickDrum);
         kick.classList.add("attack")
     }
     if (e.which == 80){
         snare.classList.add("attack")
+        createjs.Sound.play(snareDrum);
     }
 })
 
@@ -26,3 +41,5 @@ document.addEventListener("keyup", (e) =>{
         snare.classList.remove("attack")
     }
 })
+
+
